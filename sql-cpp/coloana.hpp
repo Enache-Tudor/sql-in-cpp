170419caeb26c75e2b4dc2271a46e1d307b03a62
tudor <enache.tudor25@gmail.com>
2025-12-09 16:21:17 +0200

    finished coloana class implementation added all operators for coloana

--- sql-cpp/coloana.hpp
+++ sql-cpp/coloana.hpp
@@ -7,14 +7,14 @@
 class Coloana {
 private:
 	char* nume;
-	std::vector<int>valori;
-	std::string tipDate;
+	std::vector<std::string>valori;
+	//std::string tipDate;
 	static int nrColoane;
-
+	const int idColoana;
 
 public:
 	//constructori
-	Coloana(char* nume, const std::string& tipDate);
+	Coloana(char* nume);
 	Coloana(const Coloana& c);
 	~Coloana();
 
@@ -23,11 +23,15 @@ public:
 	friend std::istream& operator>>(std::ifstream& in, Coloana& c);//dummy
 
 	Coloana operator=(const Coloana& c);
-	int& operator[](int index);
-	Coloana operator++(int); //post incrementare
-	Coloana operator++();//preincrementare
-	Coloana operator+(int valoare);
+	std::string & operator[](int index);
+	Coloana operator++(int); //post incrementare mareste valorile cu 1
+	Coloana operator++();//preincrementare mareste valorile cu 1
+	Coloana operator--(int); //post incrementare scade valorile cu 1
+	Coloana operator--();//preincrementare scade valorile cu 1
+	Coloana operator+(int valoare);//aduna o valoare numerica la toate celulele din coloana
+	Coloana operator-(int valoare);//scade o valoare numerica
 	bool operator==(const Coloana& c);
-	bool operator!() const;
-	bool operator<(const Coloana& c);
+	bool operator!() const;//returneaza true daca coloana e empty()
+	bool operator<(const Coloana& c);//compara dimensiunea coloanelor 
+	operator float() const;
 };
